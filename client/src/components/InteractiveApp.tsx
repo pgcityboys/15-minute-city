ff4d6bf336b5acf69277722363af09f551a1846b
AdamNiesiobedzki <adam.niesiobedzki@o2.pl>
2023-01-12 20:43:50 +0100

    Styling select
    :)

--- client/src/components/InteractiveApp.tsx
+++ client/src/components/InteractiveApp.tsx
@@ -5,17 +5,42 @@ import Select from "react-select";
 
 
 export function InteractiveApp(){
+    
 
+        
     const options = [
-        { value: 'green', label: 'Zielone Miasto' },
-        { value: 'innovative', label: 'Innowacyjne Miasto' },
-        { value: 'together', label: 'Wspólne Miasto' },
-        { value: 'accessible', label: 'Dostępne Miasto' },
-        { value: 'custom', label: 'Moje Miasto' }
-      ]
+            { value: 'green', label: 'Zielone Miasto' },
+            { value: 'innovative', label: 'Innowacyjne Miasto'},
+            { value: 'together', label: 'Wspólne Miasto'},
+            { value: 'accessible', label: 'Dostępne Miasto' },
+            { value: 'custom', label: 'Moje Miasto' }
+        ]
 
-    let [category, setCategory] = useState(options[0]);
+        let [category, setCategory] = useState(options[0]);
 
+        const colourStyles = {
+        
+            option: (styles, { data, isDisabled, isFocused, isSelected }, base) => {
+              // const color = chroma(data.color);
+              console.log({ data, isDisabled, isFocused, isSelected });
+              return {
+                ...styles,
+                backgroundColor: isFocused ? " #46a58a" : "#116466",
+                color: "#333333"
+              };
+            },
+            control: (base, state) => ({
+                ...base,
+                background: "#a88566",
+                // Overwrittes the different states of border
+                 borderColor: "#a88566",
+                // Removes weird border around container
+                boxShadow: state.isFocused ? null : null,
+                "&:hover": {
+                    // Overwrittes the different states of border
+                    borderColor: "#a88566"}
+              })
+          };
 
     
 
@@ -24,9 +49,14 @@ export function InteractiveApp(){
             <h1>{category.label}</h1>
             
             <Select 
+            className="react-select-container"
+            classNamePrefix="react-select"
             defaultValue={category}
             onChange={setCategory}
-            options={options} />
+            options={options}     
+            styles={colourStyles}
+
+            />
 
             <MapWithHeatmap/>
         </div>
